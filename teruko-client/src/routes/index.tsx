9d90e303b25013cd4d75ea059ad37764c730a8f7
Lichthagel <lichthagel@tuta.io>
2021-12-07 18:51:25 +0100

    show image count

--- teruko-client/src/routes/index.tsx
+++ teruko-client/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import Gallery from "../components/Gallery";
 import Nav from "../components/Nav";
 import { useSearchParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_IMAGE_COUNT } from "../queries/image";
 
 const Home = () => {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -8,8 +10,10 @@ const Home = () => {
     const tags = searchParams.getAll("tag");
     const sort = searchParams.get("sort") || "newest";
 
+    const { data: dataCount } = useQuery(GET_IMAGE_COUNT);
+
     return (
-        <div>
+        <div className="relative">
             <Nav
                 tags={tags}
                 setTags={(tags: string[]) => {
@@ -18,6 +22,8 @@ const Home = () => {
                         sort
                     });
                 }} />
+
+            {dataCount && <div className="absolute right-3 top-3 text-gray-600">{dataCount.imageCount} images</div>}
 
             <Gallery tags={tags} sort={sort as string} />
         </div>
