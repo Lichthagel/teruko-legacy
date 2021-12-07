a85f8423e61339e81f88716568d5a955db190386
Lichthagel <lichthagel@tuta.io>
2021-12-07 14:50:37 +0100

    add title to image cards

--- teruko-client/src/components/ImageCard.tsx
+++ teruko-client/src/components/ImageCard.tsx
@@ -10,11 +10,12 @@ const ImageCard: FunctionComponent<{
     filterTags?: (tag: Tag) => boolean;
 }> = ({ image, onClick, filterTags = () => true }) =>
     <div className="rounded bg-gray-700 mb-1">
-        <div onClick={onClick}>
+        <div className="relative" onClick={onClick}>
             <LoaderImage
                 src={`http://${window.location.hostname}:3030/img/${image.filename}`}
                 alt={image.title || image.id.toString()}
                 className="w-full h-auto block cursor-pointer rounded" />
+            {image.title && <div className="px-1 text-sm text-white whitespace-nowrap absolute bottom-0 left-0 right-0 z-10 bg-gradient-to-b from-transparent to-black/80 text-shadow-xl">{image.title}</div>}
         </div>
         <div className="overflow-x-scroll flex flex-row flex-nowrap scrollbar-none">
             {image.tags.filter(tag => !tag.slug.startsWith("artist_") && filterTags(tag)).map(tag =>
