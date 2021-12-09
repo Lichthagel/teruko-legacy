819513f72e64d2141c6321b2874ccb4c1b70046c
Lichthagel <lichthagel@tuta.io>
2021-12-09 20:40:01 +0100

    keep query when using home button

--- teruko-client/src/App.tsx
+++ teruko-client/src/App.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon, ChevronUpIcon, HomeIcon, PlusIcon } from "@heroicons/react/outline";
-import { Route, Routes, Link, useMatch } from "react-router-dom";
+import { Route, Routes, Link, useMatch, useSearchParams } from "react-router-dom";
 import Home from "./routes/index";
 import React, { Fragment, lazy, Suspense, useEffect, useState } from "react";
 import IconButton from "./components/IconButton";
@@ -13,6 +13,8 @@ const New = lazy(() => import("./routes/new"));
 
 const App = () => {
     const isImageView = useMatch(":id");
+
+    const [searchParams] = useSearchParams();
 
     const [showToTop, setShowToTop] = useState(false);
     const [showToBottom, setShowToBottom] = useState(true);
@@ -44,7 +46,7 @@ const App = () => {
         <div className="fixed top-2 right-0 left-0 z-20 pointer-events-none">
             <div className="container mx-auto px-3">
                 <div className={clsx("pointer-events-auto flex flex-row inline-block items-center w-min bg-dark-500 rounded-xl p-1 transition hover:opacity-100", { "opacity-20": isImageView })}>
-                    <Link to="/">
+                    <Link to={{ pathname: "/", search: `?${searchParams.toString()}` }}> {/* TODO only tags and sort */}
                         <IconButton>
                             <HomeIcon className="w-10 h-10" />
                         </IconButton>
