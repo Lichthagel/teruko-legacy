59a179ecdc1a42bcb8d9f4374cd4426d116e9716
Lichthagel <lichthagel@tuta.io>
2021-12-15 23:24:33 +0100

    edit bg color in light mode

--- teruko-client/src/routes/image/edit.tsx
+++ teruko-client/src/routes/image/edit.tsx
@@ -91,7 +91,7 @@ const EditImage = () => {
     }, [id, source, title, updateImage]);
 
     return (
-        <div className="max-w-2xl mx-auto rounded-md dark:bg-neutral-800 p-2 mt-20 shadow-md dark:shadow-indigo-800">
+        <div className="max-w-2xl mx-auto rounded-md bg-neutral-200 dark:bg-neutral-800 p-2 mt-20 shadow-md dark:shadow-indigo-800">
             <div className="flex flex-row items-center">
                 <Link to={{ pathname: `/${id}`, search: `?${searchParams.toString()}` }} replace>
                     <IconButton>
