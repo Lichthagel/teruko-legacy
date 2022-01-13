1edd89a8e069c67dc9266e00fc5ef15669f6862d
Lichthagel <lichthagel@tuta.io>
2022-01-13 12:01:47 +0100

    also delete tags when deleting image

--- teruko-server/src/resolvers/Mutation/deleteImage.ts
+++ teruko-server/src/resolvers/Mutation/deleteImage.ts
@@ -5,11 +5,28 @@ import path from "path";
 
 
 async function deleteImage(parent: void, args: ImageModel, context: Context) {
+
+    const deletedImageTags = await context.prisma.image.findUnique({ where: { id: args.id } }).tags();
+
     const deletedImage = await context.prisma.image.delete({
         where: {
             id: args.id
         }
     });
+
+    try {
+        for await (const tag of deletedImageTags) {
+            await context.prisma.$executeRaw`DELETE FROM "Tag"
+        WHERE "Tag"."slug" NOT IN (
+            SELECT DISTINCT "_ImageToTag"."B"
+            FROM "_ImageToTag"
+            WHERE "_ImageToTag"."B" = ${tag.slug}
+        )
+        AND "Tag"."slug" = ${tag.slug};`;
+        }
+    } catch (err) {
+        console.log(err);
+    }
 
     try {
         await fs.promises.rm(path.join(context.imgFolder, deletedImage.filename));
