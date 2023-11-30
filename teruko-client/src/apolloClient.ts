5336210d5f89f4fa5af02e25adbf0207e5059171
Lichthagel <hey@lichthagel.de>
2023-11-30 21:16:01 +0100

    fix(client): apollo preflight

--- teruko-client/src/apolloClient.ts
+++ teruko-client/src/apolloClient.ts
@@ -3,50 +3,63 @@ import { createUploadLink } from "apollo-upload-client";
 import { Image } from "./models";
 
 const client = new ApolloClient({
-    link: createUploadLink({
-        uri: "http://192.168.1.178:3030/graphql"
-    }),
-    cache: new InMemoryCache({
-        typePolicies: {
-            Tag: {
-                keyFields: ["slug"]
-            },
-            Query: {
-                fields: {
-                    images: {
-                        keyArgs: ["tags", "sort"],
-                        // eslint-disable-next-line @typescript-eslint/default-param-last
-                        merge(existing = [], incoming, { args }) {
-                            if (args?.sort === "random") return [...existing, ...incoming.filter((incomingImage:{ __ref: string }) => existing.findIndex((el: { __ref: string }) => el.__ref === incomingImage.__ref) === -1)];
+  link: createUploadLink({
+    uri: "http://192.168.1.178:3030/graphql",
+    headers: {
+      "Apollo-Require-Preflight": "true",
+    },
+  }),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Tag: {
+        keyFields: ["slug"],
+      },
+      Query: {
+        fields: {
+          images: {
+            keyArgs: ["tags", "sort"],
+            // eslint-disable-next-line @typescript-eslint/default-param-last
+            merge(existing = [], incoming, { args }) {
+              if (args?.sort === "random")
+                return [
+                  ...existing,
+                  ...incoming.filter(
+                    (incomingImage: { __ref: string }) =>
+                      existing.findIndex(
+                        (el: { __ref: string }) =>
+                          el.__ref === incomingImage.__ref
+                      ) === -1
+                  ),
+                ];
 
-                            const skip = args?.skip || 0;
-                            const merged = existing.slice(0);
-                            incoming.forEach((image: Image, index: number) => {
-                                merged[skip + index] = image;
-                            });
-                            return merged;
-                        }
-                    },
-                    image: {
-                        read(_, { args, toReference }) {
-                            return toReference({
-                                __typename: "Image",
-                                id: args?.id
-                            });
-                        }
-                    },
-                    tag: {
-                        read(_, { args, toReference }) {
-                            return toReference({
-                                __typename: "Tag",
-                                slug: args?.slug
-                            });
-                        }
-                    }
-                }
-            }
-        }
-    })
+              const skip = args?.skip || 0;
+              const merged = existing.slice(0);
+              incoming.forEach((image: Image, index: number) => {
+                merged[skip + index] = image;
+              });
+              return merged;
+            },
+          },
+          image: {
+            read(_, { args, toReference }) {
+              return toReference({
+                __typename: "Image",
+                id: args?.id,
+              });
+            },
+          },
+          tag: {
+            read(_, { args, toReference }) {
+              return toReference({
+                __typename: "Tag",
+                slug: args?.slug,
+              });
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
